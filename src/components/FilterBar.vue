980e85a472c4cde3b546e89fe06ce8bec9a8069a
waclukasz <wac.lukasz@gmail.com>
2020-02-24 09:41:11 +0100

    wip: base filter-bar

--- src/components/FilterBar.vue
+++ src/components/FilterBar.vue
@@ -1,64 +1,91 @@
 <template>
-  <div class="container main-filter">
-    <div class="columns">
-      <div class="column is-flex is-align-center">
-        <span class="has-text-weight-semibold has-text-grey mr-2">{{ $t('sushiAdventure') }}</span>
+  <div class="main-filter">
+    <div class="columns is-multiline">
+      <div class="column is-5-tablet">
+        <div class="is-flex is-space-between is-align-center">
+          <span class="has-text-weight-semibold has-text-grey mr-3">
+            {{ $t('sushiAdventure') }}
+          </span>
 
-        <div
-          v-for="icon in adventuresIcons"
-          :key="icon"
-          class="circle circle--lg white-bg
+          <div
+            v-for="icon in adventuresIcons"
+            :key="icon"
+            class="circle circle--lg white-bg
           is-flex is-align-center is-justify-center main-filter__adventure-icon lighter-grey-border pointer"
-        >
-          <img
-            :src="require(`@/assets/icons/${icon}.svg`)"
-            :alt="icon"
           >
+            <img
+              :src="require(`@/assets/icons/${icon}.svg`)"
+              :alt="icon"
+            >
+          </div>
         </div>
       </div>
 
-      <div class="column">
-        <div
-          class="main-filter__drop-btn is-flex is-space-between is-align-center is-relative"
-          :class="{'active': isDropdownActive}"
+      <div class="column is-flex is-12-mobile is-7-widescreen">
+        <b-dropdown
+          v-model="selectedItem"
+          class="main-filter__drop-box mr-5"
+          aria-role="list"
         >
-          <b-button
-            class="is-borderless"
-            size="is-medium"
-            :rounded="true"
-            icon-right="chevron-left"
-          />
-
-          <div
-            role="button"
-            class="main-filter__drop-trigger is-flex is-align-center is-justify-center pointer"
-            @click="isDropdownActive = !isDropdownActive"
-          >
-            <span>{{ $t(selectedItem) }}</span>
-          </div>
-
-          <b-button
-            class="is-borderless"
-            size="is-medium"
-            :rounded="true"
-            icon-right="chevron-right"
-          />
-
-          <div
-            v-if="isDropdownActive"
+          <button
+            slot="trigger"
             v-on-clickaway="closeDropdown"
-            class="main-filter__drop-box"
+            class="main-filter__drop-btn"
+            :class="{'active': isDropdownActive}"
+            type="button"
+            @click="isDropdownActive = true"
           >
-            <span
-              v-for="menuItem in filteredItems"
-              :key="menuItem"
-              class="main-filter__drop-item"
-              @click="selectMenuItem(menuItem)"
-            >
-              {{ $t(menuItem) }}
+            <span class="size-2">
+              {{ $t(selectedItem) }}
             </span>
+          </button>
+
+          <b-dropdown-item
+            v-for="listItem in menuItems"
+            :key="listItem"
+            :value="listItem"
+            aria-role="listitem"
+          >
+            {{ $t(listItem) }}
+          </b-dropdown-item>
+        </b-dropdown>
+
+        <div
+          class="main-filter__btn mr-5"
+        >
+          <span class="size-1 text-no-wrap">
+            {{ $t('sushiSets') }}
+          </span>
+          <div
+            class="is-flex is-align-center main-filter__switcher"
+          >
+            <button
+              @click.stop="substractPersonFromSet()"
+            >
+              <b-icon
+                class="main-filter__switcher-btn"
+                icon="minus-circle-outline"
+              />
+            </button>
+            <span class="text-no-wrap">
+              {{ personsQuantity }} {{ $t('personShort') }}
+            </span>
+            <button
+              @click.stop="addPersonToSet()"
+            >
+              <b-icon
+                class="main-filter__switcher-btn"
+                icon="plus-circle-outline"
+              />
+            </button>
           </div>
         </div>
+
+        <button class="main-filter__btn">
+          <span class="size-1">
+            {{ $t('diet') }}
+          </span>
+        </button>
       </div>
     </div>
   </div>
@@ -85,7 +112,9 @@ export default {
         'extras'
       ],
       isDropdownActive: false,
-      selectedItem: 'menuChoose'
+      isSetsActive: false,
+      selectedItem: 'menuChoose',
+      personsQuantity: 1
     };
   },
   computed: {
@@ -100,6 +129,17 @@ export default {
     selectMenuItem(value) {
       this.selectedItem = value;
       this.isDropdownActive = false;
+    },
+    addPersonToSet() {
+      const maxQuantity = 10;
+      if (this.personsQuantity < maxQuantity) {
+        this.personsQuantity += 1;
+      }
+    },
+    substractPersonFromSet() {
+      if (this.personsQuantity > 1) {
+        this.personsQuantity -= 1;
+      }
     }
   }
 };
